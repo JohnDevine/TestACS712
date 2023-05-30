e6ae257a2320e1a592d744701b43d67ea5fd862f
John Devine <john.h.devine@gmail.com>
2023-05-30 13:17:48 +0700

    Working with ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,67 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <JDGlobal.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const int sensorIn = 34; // pin where the OUT pin from sensor is connected on Arduino
+int mVperAmp = 185;      // this the 5A version of the ACS712 -use 100 for 20A Module and 66 for 30A Module
+int Watt = 0;
+double Voltage = 0;
+double VRMS = 0;
+double AmpsRMS = 0;
+
+// ***** function calls ******
+float getVPP()
+{
+  float result;
+  int readValue;       // value read from the sensor
+  int maxValue = 0;    // store max value here
+  int minValue = 4096; // store min value here ESP32 ADC resolution
+
+  uint32_t start_time = millis();
+  while ((millis() - start_time) < 1000) // sample for 1 Sec
+  {
+    readValue = analogRead(sensorIn);
+    // see if you have a new maxValue
+    if (readValue > maxValue)
+    {
+      /*record the maximum sensor value*/
+      maxValue = readValue;
+    }
+    if (readValue < minValue)
+    {
+      /*record the minimum sensor value*/
+      minValue = readValue;
+    }
+  }
+
+  // Subtract min from max
+  result = ((maxValue - minValue) * 3.3) / 4096.0; // ESP32 ADC resolution 4096
+  DUMP(maxValue);
+  DUMP(minValue);
+  DUMP(result);
+
+  return result;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("ACS712 current sensor");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  Serial.println("");
+  Voltage = getVPP();
+  VRMS = (Voltage / 2.0) * 0.707;             // root 2 is 0.707
+  AmpsRMS = ((VRMS * 1000) / mVperAmp) - 0.3; // 0.3 is the error I got for my sensor
+
+  Serial.print(AmpsRMS);
+  Serial.print(" Amps RMS  ---  ");
+  Watt = (AmpsRMS * 5 / 1.2);
+  // note: 1.2 is my own empirically established calibration factor
+  // as the voltage measured at D34 depends on the length of the OUT-to-D34 wire
+  // 240 is the main AC power voltage – this parameter changes locally
+  Serial.print(Watt);
+  Serial.println(" Watts");
+  delay(100);
 }
